e3ef74d2258cab0021c88b5ce44941681d987968
Naveen kumar R <16112006naveen@gmail.com>
2024-08-06 12:57:48 +0530

    Update ifelseQ2.java

--- ifelseQ2.java
+++ ifelseQ2.java
@@ -8,18 +8,7 @@ class demo{
         System.out.print("Enter your income : ");
         int a = k.nextInt();   
         
-        if(a > 7000)
-        {
-        System.out.print("Scholarship is avil");
-        }
-        else
-        {
-        System.out.print("Scholarship is not avil");
-        }
+        if(a > 7000) System.out.print("Scholarship is avil");
+        else System.out.print("Scholarship is not avil");
      }
-
-
-
-
-
 }
